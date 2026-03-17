2e7f3fee1e18c1d6a028dca2e33665a2d5701c40
aori___ <hilariomarkryan3@gmail.com>
2026-03-17 16:32:53 +0800

    dsa

---
+++ 3-16-26/1.py
@@ -0,0 +1,10 @@
+seat = ['bazar', 'vitor', 'mosqueda', 'carbonillas']
+print(seat)
+
+temp = seat[1]
+
+seat[1] = seat[3]
+print(seat)
+
+seat[3] = temp
+print(seat)
--- grades.py
+++ grades.py
@@ -8,8 +8,8 @@ first_sem = [1.2, 1.1, 1.0,1.4, 3.0]
 # Second Sem
 second_sem = [3.0, 3.4, 3.2, 2.4, 2.5]
 
-avg_first = average_grade(first_sem))
-avg._second = average_grade(second_sem)
+avg_first = average_grade(first_sem)
+avg_second = average_grade(second_sem)
 120
 print(average_grade([avg_first, avg_second]))
 
