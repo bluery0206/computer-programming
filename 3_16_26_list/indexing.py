c256d5adb96ccb7a1b0cd9b352d228d40f3450b5
aori___ <hilariomarkryan3@gmail.com>
2026-04-24 11:34:20 +0800

    (list): add initial implementations for indexing, method, and updating scripts

---
+++ 3_16_26_list/indexing.py
@@ -0,0 +1,4 @@
+#          0        1          2           3    
+seat = ['bazar', 'vitor', 'mosqueda', 'carbonillas']
+
+print(seat[0])
---
+++ 3_16_26_list/method.py
@@ -0,0 +1,13 @@
+#             0        1          2
+thislist = ["apple", "banana", "cherry"]
+
+# removes the item at index 1 (banana)
+thislist.pop(1) 
+
+# appends "caimito" at the end of the list
+thislist.append("caimito") 
+
+# inserts "manga" at index 1
+thislist.insert(1, "manga") 
+
+print(thislist)
